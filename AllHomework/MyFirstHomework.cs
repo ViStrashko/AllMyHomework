88a1d285a90a507391b825e9bdf3217fee281ba3
ViStrashko <voringir@mail.ru>
2022-03-03 04:18:17 +0200

    Adding methods homework1

---
+++ AllHomework/MyFirstHomework.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AllHomework
+{
+	static class MyFirstHomework
+	{
+		public static int GetResultOfTheEquation(int valueA, int valueB)
+		{
+			int resultCalculations;
+			resultCalculations = ((5 * valueA) + (valueB * valueB)) / (valueB - valueA);
+			return resultCalculations;
+		}
+		public static int GetVariableOfTheEquation(int valueA, int valueB, int valueC)
+		{
+			int valueX;
+			valueX = (valueC - valueB) / valueA;
+			return valueX;
+		}
+		public static void GetEquationOfTheLine(int coordinateX1, int coordinateX2, int coordinateY1, int coordinateY2)
+		{
+			int valueA = (coordinateY2 - coordinateY1) / (coordinateX2 - coordinateX1);
+			int valueB = coordinateY1 - valueA * coordinateX1;
+			Console.WriteLine($"Уравнение прямой: Y={valueA}*X+{valueB}");
+		}
+	}
+}
---
+++ AllHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AllHomework;
+
+namespace AllHomework
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int rezult=MyFirstHomework.GetResultOfTheEquation (7, 4);
+			Console.WriteLine(rezult);
+			int valueX = MyFirstHomework.GetVariableOfTheEquation (7, 5, 40);
+			Console.WriteLine(valueX);
+			MyFirstHomework.GetEquationOfTheLine (1, 5, 10, 2);
+			Console.ReadKey();
+		}
+	}
+}
